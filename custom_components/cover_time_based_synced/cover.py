cc73eebd6ec00b45ddcdfd9efc857500022972ae
Thomas Raba <t.raba@reinholds.de>
2026-02-17 22:49:03 +0100

    Add extra end-run timing for full open/close travel

--- custom_components/cover_time_based_synced/cover.py
+++ custom_components/cover_time_based_synced/cover.py
@@ -35,8 +35,11 @@ CONF_NAME = 'name'
 CONF_ALIASES = 'aliases'
 CONF_TRAVELLING_TIME_DOWN = 'travelling_time_down'
 CONF_TRAVELLING_TIME_UP = 'travelling_time_up'
+CONF_EXTRA_TIME_OPEN = 'extra_time_open'
+CONF_EXTRA_TIME_CLOSE = 'extra_time_close'
 CONF_SEND_STOP_AT_ENDS = 'send_stop_at_ends'
 DEFAULT_TRAVEL_TIME = 25
+DEFAULT_EXTRA_TIME = 0
 DEFAULT_SEND_STOP_AT_ENDS = False
 
 CONF_OPEN_SWITCH_ENTITY_ID = 'open_switch_entity_id'
@@ -62,6 +65,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
                     vol.Optional(CONF_ALIASES, default=[]): vol.All(cv.ensure_list, [cv.string]),
                     vol.Optional(CONF_TRAVELLING_TIME_DOWN, default=DEFAULT_TRAVEL_TIME): cv.positive_int,
                     vol.Optional(CONF_TRAVELLING_TIME_UP, default=DEFAULT_TRAVEL_TIME): cv.positive_int,
+                    vol.Optional(CONF_EXTRA_TIME_OPEN, default=DEFAULT_EXTRA_TIME): cv.positive_float,
+                    vol.Optional(CONF_EXTRA_TIME_CLOSE, default=DEFAULT_EXTRA_TIME): cv.positive_float,
                     vol.Optional(CONF_SEND_STOP_AT_ENDS, default=DEFAULT_SEND_STOP_AT_ENDS): cv.boolean,
                 }
             }
@@ -96,10 +101,22 @@ def devices_from_config(domain_config):
         name = config.pop(CONF_NAME)
         travel_time_down = config.pop(CONF_TRAVELLING_TIME_DOWN)
         travel_time_up = config.pop(CONF_TRAVELLING_TIME_UP)
+        extra_time_open = config.pop(CONF_EXTRA_TIME_OPEN)
+        extra_time_close = config.pop(CONF_EXTRA_TIME_CLOSE)
         open_switch_entity_id = config.pop(CONF_OPEN_SWITCH_ENTITY_ID)
         close_switch_entity_id = config.pop(CONF_CLOSE_SWITCH_ENTITY_ID)
         send_stop_at_ends = config.pop(CONF_SEND_STOP_AT_ENDS)
-        device = CoverTimeBased(device_id, name, travel_time_down, travel_time_up, open_switch_entity_id, close_switch_entity_id, send_stop_at_ends)
+        device = CoverTimeBased(
+            device_id,
+            name,
+            travel_time_down,
+            travel_time_up,
+            extra_time_open,
+            extra_time_close,
+            open_switch_entity_id,
+            close_switch_entity_id,
+            send_stop_at_ends,
+        )
         devices.append(device)
     return devices
 
@@ -121,10 +138,23 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 
 
 class CoverTimeBased(CoverEntity, RestoreEntity):
-    def __init__(self, device_id, name, travel_time_down, travel_time_up, open_switch_entity_id, close_switch_entity_id, send_stop_at_ends):
+    def __init__(
+        self,
+        device_id,
+        name,
+        travel_time_down,
+        travel_time_up,
+        extra_time_open,
+        extra_time_close,
+        open_switch_entity_id,
+        close_switch_entity_id,
+        send_stop_at_ends,
+    ):
         """Initialize the cover."""
         self._travel_time_down = travel_time_down
         self._travel_time_up = travel_time_up
+        self._extra_time_open = extra_time_open
+        self._extra_time_close = extra_time_close
         self._open_switch_entity_id = open_switch_entity_id
         self._close_switch_entity_id = close_switch_entity_id
         self._send_stop_at_ends = send_stop_at_ends
@@ -144,6 +174,15 @@ class CoverTimeBased(CoverEntity, RestoreEntity):
 
         self._switch_close_state = "off"
         self._switch_open_state = "off"
+
+    def _apply_effective_travel_times(self, target_position):
+        """Use extended travel times for full open/close end-runs if configured."""
+        effective_up = self._travel_time_up + self._extra_time_open \
+            if target_position == 100 else self._travel_time_up
+        effective_down = self._travel_time_down + self._extra_time_close \
+            if target_position == 0 else self._travel_time_down
+        self.tc.travel_time_up = effective_up
+        self.tc.travel_time_down = effective_down
 
     async def _handle_state_changed(self, event):
         if event.data.get("new_state") is None:
@@ -179,19 +218,23 @@ class CoverTimeBased(CoverEntity, RestoreEntity):
         elif self._switch_open_state == "on" and self._switch_close_state == "off":
             if self._target_position != 100 and self._target_position != 0:
                 _LOGGER.debug(self._name + ': ' + 'open/close: on/off, opening to %d', self._target_position)
+                self._apply_effective_travel_times(self._target_position)
                 self.tc.start_travel(self._target_position)
             else:
                 _LOGGER.debug(self._name + ': ' + 'open/close: on/off, opening to predefined 100')
                 self._target_position = 100
+                self._apply_effective_travel_times(self._target_position)
                 self.tc.start_travel_up()
             self.start_auto_updater()
         elif self._switch_open_state == "off" and self._switch_close_state == "on":
             if self._target_position != 100 and self._target_position != 0:
                 _LOGGER.debug(self._name + ': ' + 'open/close: on/off, closing to %d', self._target_position)
+                self._apply_effective_travel_times(self._target_position)
                 self.tc.start_travel(self._target_position)
             else:
                 _LOGGER.debug(self._name + ': ' + 'open/close: on/off, closing to predefined 0')
                 self._target_position = 0
+                self._apply_effective_travel_times(self._target_position)
                 self.tc.start_travel_down()
             self.start_auto_updater()
             
@@ -241,6 +284,8 @@ class CoverTimeBased(CoverEntity, RestoreEntity):
             attr[CONF_TRAVELLING_TIME_DOWN] = self._travel_time_down
         if self._travel_time_up is not None:
             attr[CONF_TRAVELLING_TIME_UP] = self._travel_time_up 
+        attr[CONF_EXTRA_TIME_OPEN] = self._extra_time_open
+        attr[CONF_EXTRA_TIME_CLOSE] = self._extra_time_close
         attr[ATTR_UNCONFIRMED_STATE] = str(self._assume_uncertain_position)
         return attr
 
@@ -308,6 +353,7 @@ class CoverTimeBased(CoverEntity, RestoreEntity):
             command = SERVICE_OPEN_COVER
         if command is not None:
             self.start_auto_updater()
+            self._apply_effective_travel_times(position)
             self.tc.start_travel(position)
             _LOGGER.debug(self._name + ': ' + 'set_position :: command %s', command)
             await self._async_handle_command(command)
@@ -353,9 +399,11 @@ class CoverTimeBased(CoverEntity, RestoreEntity):
           self._handle_my_button()
           return
         if action == "open":
+          self._apply_effective_travel_times(100)
           self.tc.start_travel_up()
           self._target_position = 100
         if action == "close":
+          self._apply_effective_travel_times(0)
           self.tc.start_travel_down()
           self._target_position = 0
         self.start_auto_updater()
@@ -377,10 +425,12 @@ class CoverTimeBased(CoverEntity, RestoreEntity):
 
         if self.tc.is_traveling():
           self.tc.set_position(position)
+          self._apply_effective_travel_times(self._target_position)
           self.tc.start_travel(self._target_position)
           self.start_auto_updater()
         else:
           if position_type == ATTR_POSITION_TYPE_TARGET:
+            self._apply_effective_travel_times(self._target_position)
             self.tc.start_travel(self._target_position)
             self.start_auto_updater()
           else:
